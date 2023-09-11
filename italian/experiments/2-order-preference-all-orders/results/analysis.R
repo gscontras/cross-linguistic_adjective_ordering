f799b367585645e1a67367351e792e46bf9d7e82
Greg Scontras <gscontras@gmail.com>
2023-09-11 14:01:45 -0700

    results

--- italian/experiments/2-order-preference-all-orders/results/analysis.R
+++ italian/experiments/2-order-preference-all-orders/results/analysis.R
@@ -16,9 +16,10 @@ d$workerid = d$participant_id
 unique(d$language)
 
 all <- d
-# only native Vietnamese speakers (n=1)
+# only native Italian speakers
 d = d[d$language!="GREG"
-      &d$language !="Spagnolo Italiano",]
+      &d$language !="Spagnolo Italiano"
+      &d$language !="",]
 
 length(unique(d$workerid)) # n=110 (120)
 
@@ -50,19 +51,24 @@ nrow(agr) #5720
 adj_agr = aggregate(correctresponse~predicate*correctclass*condition,FUN=mean,data=agr)
 adj_agr
 
-class_agr = aggregate(correctresponse~correctclass*condition,FUN=mean,data=agr)
+#class_agr = aggregate(correctresponse~correctclass*condition,FUN=mean,data=agr)
+source("helpers.R")
+class_agr = bootsSummary(data=agr, measurevar="correctresponse", groupvars=c("correctclass","condition"))
+
 
 ggplot(data=class_agr,aes(x=reorder(correctclass,-correctresponse,mean),y=correctresponse))+
-  geom_bar(stat="identity")+
-  #geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(class1,-correctresponse,mean), width=0.1),alpha=0.5)+
-  xlab("\nadjective class")+
-  ylab("preference for first position\n")+
+  geom_bar(stat="identity",fill="lightgray",color="black")+
+  geom_hline(yintercept=0.5,linetype="dashed") + 
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(correctclass,-correctresponse,mean), width=0.25),alpha=1)+
+  xlab("adjective class")+
+  ylab("preference\nfor first position\n")+
   ylim(0,1)+
   facet_grid(.~condition)+
   #labs("order\npreference")+
-  theme_bw()#+
-  #theme(axis.text.x=element_text(angle=90,vjust=0.35,hjust=1))
-#ggsave("class_distance.pdf",height=3)
+  theme_bw()+
+  theme(axis.text.x=element_text(angle=45,vjust=1,hjust=1))
+#theme(axis.text.x=element_text(angle=90,vjust=0.35,hjust=1))
+#ggsave("class_distance.png",height=2,width=7)
 
 
 
